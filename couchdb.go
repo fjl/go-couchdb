acc391f5c26f374c9b4d4a7b6497decafdee2bbd
Felix Lange <fjl@twurst.com>
2014-02-03 16:56:56 +0100

    couchdb: use generic error handling also for HEAD requests

--- couchdb.go
+++ couchdb.go
@@ -150,11 +150,7 @@ func (srv *Server) URL() string {
 // Ping can be used to check whether a server is alive.
 // It sends an HTTP HEAD request to the server's URL.
 func (srv *Server) Ping() error {
-	req, err := srv.newRequest("HEAD", "/", nil)
-	if err != nil {
-		return err
-	}
-	_, err = srv.http.Do(req)
+	_, err := srv.closedRequest("HEAD", "/", nil)
 	return err
 }
 
@@ -338,13 +334,19 @@ type DatabaseError struct {
 	Method     string // HTTP method of the request
 	URL        string // HTTP URL of the request
 	StatusCode int    // HTTP status code of the response
-	ErrorCode  string // Error reason provided by CouchDB
-	Reason     string // Error message provided by CouchDB
+
+	// These two fields will be empty for HEAD requests.
+	ErrorCode string // Error reason provided by CouchDB
+	Reason    string // Error message provided by CouchDB
 }
 
 func (e DatabaseError) Error() string {
-	return fmt.Sprintf("%v %v: (%v) %v: %v",
-		e.Method, e.URL, e.StatusCode, e.ErrorCode, e.Reason)
+	if e.ErrorCode == "" {
+		return fmt.Sprintf("%v %v: %v", e.Method, e.URL, e.StatusCode)
+	} else {
+		return fmt.Sprintf("%v %v: (%v) %v: %v",
+			e.Method, e.URL, e.StatusCode, e.ErrorCode, e.Reason)
+	}
 }
 
 // Convenience function that checks whether the given error
@@ -375,10 +377,11 @@ func ErrorStatus(err error, statusCode int) bool {
 
 func dbError(resp *http.Response) error {
 	var reply struct{ Error, Reason string }
-	if err := readBody(resp, &reply); err != nil {
-		return fmt.Errorf("couldn't decode CouchDB error: %v", err)
+	if resp.Request.Method != "HEAD" {
+		if err := readBody(resp, &reply); err != nil {
+			return fmt.Errorf("couldn't decode CouchDB error: %v", err)
+		}
 	}
-
 	return DatabaseError{
 		Method:     resp.Request.Method,
 		URL:        resp.Request.URL.String(),
