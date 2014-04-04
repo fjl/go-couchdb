c39fc82e1dbebff074753cc5012f5f462a397c45
Felix Lange <fjl@twurst.com>
2014-04-04 04:11:32 +0200

    couchdaemon: API cleanup
    - added ConfigSection for retrieving whole sections
    - added functions for level-based logging
    - errors are propagated through the API
    - the functions no longer hang when Init hasn’t been called

--- couchdaemon/couchdaemon.go
+++ couchdaemon/couchdaemon.go
@@ -3,114 +3,194 @@
 package couchdaemon
 
 import (
-	"bytes"
+	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	_ "regexp"
 	"strings"
+	"sync"
 )
 
-// TODO: the implementation is a bit hard to test due to the
-// globals, but I don't feel like changing that.
+var (
+	initOnce sync.Once
+)
 
 var (
-	reqchan  = make(chan request)
-	respchan = make(chan string, 1)
+	// ErrNotFound is returned by the config API when a key is not available.
+	ErrNotFound = errors.New("couchdaemon: config key not found")
+
+	// ErrNotInitialized is returned by all API functions
+	// before Init has been called.
+	ErrNotInitialized = errors.New("couchdaemon: not initialized")
 )
 
 // Init configures stdin and stdout for communication with couchdb.
 //
-// The argument can be a writable channel or nil.
-// If it is nil, the process will exit with status 0
-// when CouchDB signals that is exiting by closing stdin.
-// If it is a channel, the channel will be closed instead.
+// The argument can be a writable channel or nil. If it is nil, the process
+// will exit with status 0 when CouchDB signals that is exiting. If the value
+// is a channel, the channel will be closed instead.
 //
-// You should call this function early in your initialization
-// Using stdio after Init has been called will confuse the
-// implementation and should therefore be avoided.
-// You should also refrain from calling Init more than once.
+// Stdin or stdout directly will confuse CouchDB should therefore be avoided.
 //
-// The other API functions will hang until Init has been called.
-func Init(exit chan<- bool) {
-	if exit == nil {
-		start(os.Stdin, os.Stdout, func() { os.Exit(0) })
-	} else {
-		start(os.Stdin, os.Stdout, func() { close(exit) })
+// You should call this function early in your initialization.
+// The other API functions will return ErrNotInitialized until Init
+// has been called.
+func Init(exit chan<- struct{}) {
+	initOnce.Do(func() {
+		if exit == nil {
+			start(os.Stdin, os.Stdout, func() { os.Exit(0) })
+		} else {
+			start(os.Stdin, os.Stdout, func() { os.Exit(0) })
+		}
+	})
+}
+
+// ConfigSection reads a whole section from the CouchDB configuration.
+// If the section is not present, the error will be ErrNotFound and
+// the returned map will be nil.
+func ConfigSection(section string) (map[string]string, error) {
+	var val *map[string]string
+	err := request(&val, "get", section)
+	switch {
+	case err != nil:
+		return nil, err
+	case val == nil:
+		return nil, ErrNotFound
+	default:
+		return *val, nil
 	}
 }
 
-// The tests use this function to check everything without using stdio.
-func start(stdin io.Reader, stdout io.Writer, exit func()) {
-	go writeloop(stdout)
-	go readloop(stdin, exit)
-}
-
-// Config reads a parameter value from the couchdb configuration.
-// The path elements are separated by '/', e.g. "vendor/version".
-// The returned string will be empty if the parameter is unset.
-func Config(path string) string {
-	req := request{readresp: true, query: strings.Split("get/"+path, "/")}
-	reqchan <- req
-	return <-respchan
+// ConfigVal reads a parameter value from the CouchDB configuration.
+// If the parameter is unset, the error will be ErrNotFound and the
+// returned string will be empty.
+func ConfigVal(section, item string) (string, error) {
+	var val *string
+	err := request(&val, "get", section, item)
+	switch {
+	case err != nil:
+		return "", err
+	case val == nil:
+		return "", ErrNotFound
+	default:
+		return *val, nil
+	}
 }
 
 // ServerURL returns the URL of the CouchDB server that started the daemon.
-func ServerURL() string {
-	addr := Config("httpd/bind_address")
+func ServerURL() (string, error) {
+	port, err := ConfigVal("httpd", "port")
+	if err != nil {
+		return "", err
+	}
+	addr, err := ConfigVal("httpd", "bind_address")
+	if err != nil {
+		return "", err
+	}
 	if addr == "0.0.0.0" {
 		addr = "127.0.0.1"
 	}
-	return "http://" + addr + ":" + Config("httpd/port") + "/"
+	return "http://" + addr + ":" + port + "/", nil
 }
 
-// Log creates a writer that outputs to the CouchDB log.
-//
-// The returned writer is threadsafe and therefore suitable
-// as an input to log.SetOutput()
-func Log() io.Writer {
-	return &logger{}
+// A LogWriter writes messages to the CouchDB log.
+// Its method set is a subset of the methods provided by log/syslog.Writer.
+type LogWriter interface {
+	io.Writer
+	// Err writes a message with level "error"
+	Err(msg string) error
+	// Info writes a message with level "info"
+	Info(msg string) error
+	// Info writes a message with level "debug"
+	Debug(msg string) error
 }
 
 type logger struct{}
 
-func (l *logger) Write(msg []byte) (int, error) {
-	logmsg := bytes.TrimRight(msg, "\n")
-	reqchan <- request{query: []string{"log", string(logmsg)}}
-	<-respchan // wait until the message has been sent
+// NewLogWriter creates a log writer that outputs to the CouchDB log.
+func NewLogWriter() LogWriter { return logger{} }
+
+func (logger) Err(msg string) error   { return logwrite(msg, &optsError) }
+func (logger) Info(msg string) error  { return logwrite(msg, &optsInfo) }
+func (logger) Debug(msg string) error { return logwrite(msg, &optsDebug) }
+
+func (logger) Write(msg []byte) (int, error) {
+	if err := logwrite(string(msg), nil); err != nil {
+		return 0, err
+	}
 	return len(msg), nil
 }
 
-type request struct {
-	query    []string
-	readresp bool
-}
+var (
+	optsError = json.RawMessage(`{"level":"error"}`)
+	optsInfo  = json.RawMessage(`{"level":"info"}`)
+	optsDebug = json.RawMessage(`{"level":"debug"}`)
+)
 
-func writeloop(stdout io.Writer) {
-	out := json.NewEncoder(stdout)
-	for req := range reqchan {
-		out.Encode(req.query)
-		if !req.readresp {
-			// unblock caller
-			respchan <- ""
-		}
+func logwrite(msg string, opts *json.RawMessage) error {
+	msg = strings.TrimRight(msg, "\n")
+	if opts == nil {
+		return request(nil, "log", msg)
 	}
+	return request(nil, "log", msg, opts)
 }
 
-func readloop(stdin io.Reader, exit func()) {
-	dec := json.NewDecoder(stdin)
+var (
+	// mutex protects the globals during initialization and request I/O
+	mutex sync.Mutex
+
+	exit   func()
+	stdin  io.ReadCloser
+	stdout io.Writer
+	inputc chan []byte
+)
+
+func start(in io.ReadCloser, out io.Writer, ef func()) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	exit = ef
+	stdin = in
+	stdout = out
+	inputc = make(chan []byte)
+	go inputloop(in, inputc, exit)
+}
+
+// inputloop reads lines from stdin until it is closed.
+func inputloop(in io.Reader, inputc chan<- []byte, exit func()) {
+	bufin := bufio.NewReader(in)
 	for {
-		var resp string
-		err := dec.Decode(&resp)
+		line, err := bufin.ReadBytes('\n')
 		if err != nil {
-			if err != io.EOF {
-				fmt.Fprintf(os.Stderr, "couchdaemon: response decoding error: %v\n", err)
-			}
-			respchan <- ""
-			exit()
-			return
-		} else {
-			respchan <- resp
+			break
+		}
+		inputc <- line
+	}
+	exit()
+	close(inputc)
+}
+
+func request(result interface{}, query ...interface{}) error {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if exit == nil {
+		return ErrNotInitialized
+	}
+	line, err := json.Marshal(query)
+	if err != nil {
+		return err
+	}
+	if _, err := fmt.Fprintf(stdout, "%s\n", line); err != nil {
+		return err
+	}
+	if result != nil {
+		if err := json.Unmarshal(<-inputc, result); err != nil {
+			return err
 		}
 	}
+	return nil
 }
--- couchdaemon/couchdaemon_test.go
+++ couchdaemon/couchdaemon_test.go
@@ -1,26 +1,111 @@
 package couchdaemon
 
 import (
+	"bufio"
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"io"
+	"reflect"
 	"testing"
 	"time"
 )
 
-var (
-	stdout         = new(bytes.Buffer)
-	stdin, stdin_w = io.Pipe()
-	exitchan       = make(chan bool)
-)
-
-func init() {
-	start(stdin, stdout, func() { exitchan <- true })
+type testHost struct {
+	output   *bytes.Buffer
+	exitchan chan struct{}
+	config   testConfig
+	outW     io.Closer
 }
 
-func TestLog(t *testing.T) {
-	defer stdout.Reset()
-	log := Log()
+type testConfig map[string]map[string]string
+
+func startTestHost(t *testing.T, config testConfig) *testHost {
+	inR, inW := io.Pipe()   // input stream (testHost writes, daemon reads)
+	outR, outW := io.Pipe() // output stream (testHost reads, daemon writes)
+	th := &testHost{
+		output:   new(bytes.Buffer),
+		exitchan: make(chan struct{}),
+		config:   config,
+		outW:     outW,
+	}
+
+	go func() {
+		enc := json.NewEncoder(inW)
+		bufoutR := bufio.NewReader(outR)
+		var req []interface{}
+		for {
+			line, err := bufoutR.ReadBytes('\n')
+			if err != nil {
+				return
+			}
+
+			th.output.Write(line)
+			if err := json.Unmarshal(line, &req); err != nil {
+				t.Errorf("testHost: could not decode request: %v", err)
+				return
+			}
+
+			t.Logf("testHost: got request %v", req)
+			switch {
+			case len(req) <= 1:
+				t.Errorf("request array to short")
+			case req[0] == "log":
+				break
+			case req[0] == "get" && req[1] == "garbage":
+				io.WriteString(inW, "garbage line\n")
+			case req[0] == "get" && len(req) == 2:
+				enc.Encode(config[req[1].(string)])
+			case req[0] == "get" && len(req) == 3:
+				if v, found := config[req[1].(string)][req[2].(string)]; found {
+					enc.Encode(v)
+				} else {
+					enc.Encode(nil)
+				}
+			default:
+				t.Errorf("testHost: unmatched request")
+			}
+		}
+	}()
+
+	start(inR, outW, func() { close(th.exitchan) })
+	return th
+}
+
+func (th *testHost) stop() {
+	th.outW.Close()
+	stdin.Close()
+}
+
+func TestNotInitialized(t *testing.T) {
+	if _, err := ConfigSection("s"); err != ErrNotInitialized {
+		t.Errorf("ConfigSection err mismatch, got %v, want ErrNotInitialized")
+	}
+	if _, err := ConfigVal("s", "k"); err != ErrNotInitialized {
+		t.Errorf("ConfigVal err mismatch, got %v, want ErrNotInitialized")
+	}
+	if _, err := ServerURL(); err != ErrNotInitialized {
+		t.Errorf("ServerURL err mismatch, got %v, want ErrNotInitialized")
+	}
+	log := NewLogWriter()
+	if _, err := log.Write([]byte("foo")); err != ErrNotInitialized {
+		t.Errorf("log.Write err mismatch, got %v, want ErrNotInitialized")
+	}
+	if err := log.Err("foo"); err != ErrNotInitialized {
+		t.Errorf("log.Err err mismatch, got %v, want ErrNotInitialized")
+	}
+	if err := log.Info("foo"); err != ErrNotInitialized {
+		t.Errorf("log.Info err mismatch, got %v, want ErrNotInitialized")
+	}
+	if err := log.Debug("foo"); err != ErrNotInitialized {
+		t.Errorf("log.Debug err mismatch, got %v, want ErrNotInitialized")
+	}
+}
+
+func TestLogWrite(t *testing.T) {
+	th := startTestHost(t, nil)
+	defer th.stop()
+
+	log := NewLogWriter()
 	msg := "a\"bc\n"
 
 	n, err := io.WriteString(log, msg)
@@ -30,47 +115,168 @@ func TestLog(t *testing.T) {
 	if n != len(msg) {
 		t.Errorf("short write: %v != %v", n, len(msg))
 	}
-	if stdout.String() != `["log","a\"bc"]`+"\n" {
-		t.Errorf("wrong JSON output: %q", stdout.String())
+	if th.output.String() != `["log","a\"bc"]`+"\n" {
+		t.Errorf("wrong JSON output: %s", th.output.String())
 	}
 }
 
-func TestConfig(t *testing.T) {
-	expVal := "12345678"
+func TestLogWriteError(t *testing.T) {
+	th := startTestHost(t, nil)
+	defer th.stop()
 
-	defer stdout.Reset()
-	go func() { fmt.Fprintf(stdin_w, "%q\n", expVal) }()
+	th.outW.Close()
 
-	val := Config("a/b/c")
-	if val != expVal {
-		t.Errorf("result value mismatch: want %q, got: %q", expVal, val)
+	log := NewLogWriter()
+	if _, err := log.Write([]byte("msg")); err != io.ErrClosedPipe {
+		t.Errorf(`log.Write("msg") err mismatch, got %v, want io.ErrClosedPipe`)
 	}
-	if stdout.String() != `["get","a","b","c"]`+"\n" {
-		t.Errorf("wrong JSON output: %q", stdout.String())
+}
+
+func TestLogLevels(t *testing.T) {
+	th := startTestHost(t, nil)
+	defer th.stop()
+
+	log := NewLogWriter()
+	cases := []struct {
+		method func(string) error
+		output string
+	}{
+		{log.Err, `["log","msg",{"level":"error"}]`},
+		{log.Info, `["log","msg",{"level":"info"}]`},
+		{log.Debug, `["log","msg",{"level":"debug"}]`},
+	}
+
+	for _, testcase := range cases {
+		th.output.Reset()
+		if err := testcase.method("msg"); err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if th.output.String() != testcase.output+"\n" {
+			t.Errorf("wrong JSON output: %s", th.output.String())
+		}
+	}
+}
+
+func TestConfigVal(t *testing.T) {
+	th := startTestHost(t, testConfig{
+		"a": {"b": "12345678"},
+	})
+	defer th.stop()
+
+	expVal := th.config["a"]["b"]
+	val, err := ConfigVal("a", "b")
+	if err != nil {
+		t.Fatalf("unexpected error")
+	}
+	if val != expVal {
+		t.Errorf(`ConfigVal("a", "b") got: %q, want: %q`, val, expVal)
+	}
+	if th.output.String() != `["get","a","b"]`+"\n" {
+		t.Errorf("wrong JSON output: %q", th.output.String())
+	}
+}
+
+func TestConfigValNotFound(t *testing.T) {
+	th := startTestHost(t, nil)
+	defer th.stop()
+
+	val, err := ConfigVal("missing-s", "missing-k")
+	if val != "" {
+		t.Errorf(`ConfigVal("missing-s", "missing-k") got: %q, want: ""`, val)
+	}
+	if err != ErrNotFound {
+		t.Errorf(`ConfigVal("missing-s", "missing-k") got err: %v, want: ErrNotFound`, err)
+	}
+}
+
+func TestConfigValDecodeError(t *testing.T) {
+	th := startTestHost(t, nil)
+	defer th.stop()
+
+	_, err := ConfigVal("garbage", "key")
+	t.Logf("err: %+v", err)
+	if err == nil {
+		t.Fatalf("expected error but no error was returned")
+	}
+}
+
+func TestConfigSection(t *testing.T) {
+	th := startTestHost(t, testConfig{
+		"section1": {
+			"a": "value-of-a",
+			"b": "value-of-b",
+			"c": "value-of-c",
+		},
+		"section2": {
+			"a": "value-of-a-in-section-2",
+		},
+	})
+	defer th.stop()
+
+	expVal := th.config["section1"]
+	val, err := ConfigSection("section1")
+	if err != nil {
+		t.Fatalf(`ConfigSection("section1") returned an error: %v`, err)
+	}
+	if !reflect.DeepEqual(val, expVal) {
+		t.Errorf(`ConfigSection("section1") got: %v, want %v`, val, expVal)
+	}
+	if th.output.String() != `["get","section1"]`+"\n" {
+		t.Errorf("wrong JSON output: %q", th.output.String())
+	}
+}
+
+func TestConfigSectionNotFound(t *testing.T) {
+	th := startTestHost(t, nil)
+	defer th.stop()
+
+	val, err := ConfigSection("missing")
+	if val != nil {
+		t.Errorf(`ConfigSection("missing") got: %q, want: ""`, val)
+	}
+	if err != ErrNotFound {
+		t.Errorf(`ConfigSection("missing") got err: %v, want: ErrNotFound`, err)
+	}
+}
+
+func TestConfigSectionDecodeError(t *testing.T) {
+	th := startTestHost(t, nil)
+	defer th.stop()
+
+	val, err := ConfigSection("garbage")
+	if err == nil {
+		t.Errorf(`ConfigSection("garbage") should've returned an error`)
+	}
+	if val != nil {
+		t.Errorf(`ConfigSection("garbage") got: %v, want nil`, val)
 	}
 }
 
 func TestServerURL(t *testing.T) {
-	url := "http://127.0.0.1:5984/"
+	th := startTestHost(t, testConfig{
+		"httpd": {
+			"bind_address": "127.0.0.1",
+			"port":         "5984",
+		},
+	})
+	defer th.stop()
 
-	// queue config response
-	defer stdout.Reset()
-	go func() {
-		io.WriteString(stdin_w, `"127.0.0.1"`+"\n")
-		io.WriteString(stdin_w, `"5984"`+"\n")
-	}()
-
-	respurl := ServerURL()
-	if respurl != url {
-		t.Errorf("wrong URL returned: %q != %q", respurl, url)
+	expVal := "http://127.0.0.1:5984/"
+	respurl, err := ServerURL()
+	if err != nil {
+		t.Fatalf("ServerURL() returned error: %v", err)
+	}
+	if respurl != expVal {
+		t.Errorf("ServerURL() mismatch: got %q, want %q", respurl, expVal)
 	}
 }
 
-// this has to be the last test because it closes the pipe
 func TestExit(t *testing.T) {
-	stdin_w.Close()
+	th := startTestHost(t, nil)
+	th.stop()
+
 	select {
-	case <-exitchan:
+	case <-th.exitchan:
 		return
 	case <-time.After(200 * time.Millisecond):
 		t.Error("exit func has not been called")
