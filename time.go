fd5f01fb5bc2ffc3808204c6f89e51c6c36e838d
Sam Lown <me@samlown.com>
2018-01-25 13:02:08 -0600

    Adding support for times with zone including JSON marshaling

--- time.go
+++ time.go
@@ -9,7 +9,7 @@ import (
 const (
 	TimeFormat         = "2006-01-02T15:04:05.000Z"
 	TimeFormatShort    = "2006-01-02T15:04:05Z"
-	TimeFormatTimeZone = "2006-01-02T15:04:15Z-0700"
+	TimeFormatTimeZone = "2006-01-02T15:04:05-0700"
 )
 
 // TimeNow return a new CouchTime with current time
@@ -19,6 +19,12 @@ func TimeNow() Time {
 
 // Time is used to decode from json times from CouchDB
 type Time struct {
+	time.Time
+}
+
+// TimeWithZone allows times with a zone to be persisted to the database.
+// Very useful storing documents in a local time.
+type TimeWithZone struct {
 	time.Time
 }
 
@@ -50,8 +56,35 @@ func (t Time) MarshalJSON() ([]byte, error) {
 	return []byte(`"` + t.String() + `"`), nil
 }
 
+// UnmarshalJSON allows type to be passed to json.Unmarshal
+func (t *TimeWithZone) UnmarshalJSON(data []byte) error {
+	s := string(data)
+	if s == "null" {
+		return nil
+	}
+	c, err := time.Parse(`"`+TimeFormatTimeZone+`"`, s)
+	if err != nil {
+		return fmt.Errorf("unable to parse time with zone. %s", err)
+	}
+	t.Time = c
+	return nil
+}
+
+// MarshalJSON allows type to be passed to json.MarshalJSON
+func (t TimeWithZone) MarshalJSON() ([]byte, error) {
+	if t.IsNull() {
+		return []byte(`null`), nil
+	}
+	return []byte(`"` + t.String() + `"`), nil
+}
+
 // IsNull determines if a time value is set or not
 func (t *Time) IsNull() bool {
+	return t.Equal(nullTime)
+}
+
+// IsNull determines if a time value is set or not
+func (t *TimeWithZone) IsNull() bool {
 	return t.Equal(nullTime)
 }
 
@@ -61,6 +94,13 @@ func ParseTime(timeString string) (Time, error) {
 	return Time{o}, err
 }
 
+// ParseTimeWithZone reads the provided ISO time string and creates a time object
+// which references the zone.
+func ParseTimeWithZone(timeString string) (TimeWithZone, error) {
+	o, err := time.Parse(TimeFormatTimeZone, timeString)
+	return TimeWithZone{o}, err
+}
+
 // String outputs back time in CouchDB format
 func (t *Time) String() string {
 	return t.Format(TimeFormat)
@@ -68,5 +108,10 @@ func (t *Time) String() string {
 
 // StringWithTimeZone outputs the time including a timezone
 func (t *Time) StringWithTimeZone() string {
+	return t.Format(TimeFormatTimeZone)
+}
+
+// String provides the text version of the time with the zone included.
+func (t *TimeWithZone) String() string {
 	return t.Format(TimeFormatTimeZone)
 }
--- time_test.go
+++ time_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func TestUnmarshal(t *testing.T) {
+func TestTimeUnmarshal(t *testing.T) {
 	var cases = []struct {
 		Given    string
 		Expected time.Time
@@ -32,7 +32,7 @@ func TestUnmarshal(t *testing.T) {
 	}
 }
 
-func TestMarshal(t *testing.T) {
+func TestTimeMarshal(t *testing.T) {
 	var cases = []struct {
 		Given    time.Time
 		Expected string
@@ -53,9 +53,70 @@ func TestMarshal(t *testing.T) {
 	}
 }
 
+func TestTimeNow(t *testing.T) {
+	ct := TimeNow()
+	if z, _ := ct.Zone(); z != "UTC" {
+		t.Errorf("Failed to get current time in UTC, got: %v", z)
+	}
+}
+
 func TestIsNull(t *testing.T) {
 	ct := Time{}
 	if !ct.IsNull() {
-		t.Error("Expected empty CouchTime to be null")
+		t.Error("Expected empty Couch Time to be null")
+	}
+}
+
+func TestIsNullWithZone(t *testing.T) {
+	ct := TimeWithZone{}
+	if !ct.IsNull() {
+		t.Error("Expected empty Couch TimeWithZone to be null")
+	}
+}
+
+func TestTimeWithZoneUnmarshal(t *testing.T) {
+	tl, _ := time.LoadLocation("America/Mexico_City")
+	var cases = []struct {
+		Given    string
+		Expected time.Time
+		Error    bool
+	}{
+		// long form
+		{`"2009-11-10T23:19:45-0600"`, time.Date(2009, time.November, 10, 23, 19, 45, 0, tl), false},
+		// bad form
+		{`"Z2009-11-10T13:19:04Z"`, time.Time{}, true},
+	}
+
+	for _, c := range cases {
+		payload := []byte(c.Given)
+		var output TimeWithZone
+		if err := json.Unmarshal(payload, &output); err != nil && !c.Error {
+			t.Error(err)
+		}
+		if !output.Equal(c.Expected) {
+			t.Errorf("Expected: %q, Given: %q", c.Expected, output)
+		}
+	}
+}
+
+func TestTimeWithZoneMarshal(t *testing.T) {
+	tl, _ := time.LoadLocation("America/Mexico_City")
+	var cases = []struct {
+		Given    time.Time
+		Expected string
+	}{
+		{time.Date(2009, time.November, 10, 23, 19, 30, 0, tl), `"2009-11-10T23:19:30-0600"`},
+		{time.Date(2009, time.November, 10, 13, 19, 4, 0, tl), `"2009-11-10T13:19:04-0600"`},
+	}
+
+	for _, c := range cases {
+		ct := TimeWithZone{c.Given}
+		output, err := json.Marshal(ct)
+		if err != nil {
+			t.Error(err)
+		}
+		if string(output) != c.Expected {
+			t.Errorf("Expected: %q, Given: %q", c.Expected, output)
+		}
 	}
 }
